bceb285ae90cf63cc66a2b44a51ec6bcce126e6e
prankstah20 <pranav.karmarkar@gmail.com>
2026-04-27 00:02:56 -0500

    Revert "made it impossible to TOSS items"
    This reverts commit d6ae286fd25873efca48e8e896126bf94fc04d23.

--- src/item_menu.c
+++ src/item_menu.c
@@ -293,7 +293,8 @@ static const struct MenuAction sItemMenuActions[] = {
 // these are all 2D arrays with a width of 2 but are represented as 1D arrays
 // ACTION_DUMMY is used to represent blank spaces
 static const u8 sContextMenuItems_ItemsPocket[] = {
-    ACTION_GIVE,        ACTION_CANCEL
+    ACTION_USE,         ACTION_GIVE,
+    ACTION_TOSS,        ACTION_CANCEL
 };
 
 static const u8 sContextMenuItems_KeyItemsPocket[] = {
@@ -302,7 +303,8 @@ static const u8 sContextMenuItems_KeyItemsPocket[] = {
 };
 
 static const u8 sContextMenuItems_BallsPocket[] = {
-    ACTION_GIVE,        ACTION_CANCEL
+    ACTION_GIVE,        ACTION_DUMMY,
+    ACTION_TOSS,        ACTION_CANCEL
 };
 
 static const u8 sContextMenuItems_TmHmPocket[] = {
@@ -311,7 +313,9 @@ static const u8 sContextMenuItems_TmHmPocket[] = {
 };
 
 static const u8 sContextMenuItems_BerriesPocket[] = {
-    ACTION_GIVE,        ACTION_CANCEL
+    ACTION_CHECK_TAG,   ACTION_DUMMY,
+    ACTION_USE,         ACTION_GIVE,
+    ACTION_TOSS,        ACTION_CANCEL
 };
 
 static const u8 sContextMenuItems_BattleUse[] = {
